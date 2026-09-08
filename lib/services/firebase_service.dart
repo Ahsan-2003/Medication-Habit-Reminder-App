2d4323f38dd8c4197f972dd42e347b7a39355a4c
Ahsan <ahsanlma5e@gmail.com>
2026-09-08 21:39:20 +0300

    Add firebase service file

---
+++ lib/services/firebase_service.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseService {
+  static final FirebaseService _instance = FirebaseService._internal();
+  factory FirebaseService() => _instance;
+  FirebaseService._internal();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  FirebaseAuth get auth => _auth;
+  FirebaseFirestore get firestore => _firestore;
+
+  static Future<void> initialize() async {
+    await Firebase.initializeApp();
+  }
+}
